0b51100945e5d984d845d9187efb86d0d8a447a8
Chintan Changole <chintanchangole@gmail.com>
2026-09-05 21:34:19 +0200

    add region_summary mart for leadership reporting

---
+++ dbt_project/models/region_summary.sql
@@ -0,0 +1,10 @@
+SELECT 
+    region,
+    COUNT(DISTINCT hcp_id) AS active_hcps,
+    SUM(total_calls) AS total_calls,
+    ROUND(SUM(total_calls) :: FLOAT / COUNT(DISTINCT hcp_id), 1) AS avg_calls_per_hcp
+FROM {{ ref('hcp_engagement') }}
+GROUP BY region
+ORDER BY total_calls DESC
+
+SELECT * FROM SANOFI_PIPELINE.DBT_DEV.REGION_SUMMARY;
